bcb3ec6aa18a2adb9bcfda371429c7f3c907a58d
iryston <iryston58@gmail.com>
2022-08-30 23:15:15 +0200

    refactor

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,16 @@
-$(function(){
-    $('.header__slider').slick({
-        arrows: false,
-        vertical: true,
-        dots: true,
-        // autoplay: true,
-        lazyLoad: 'progressive',
-        responsive: true,
-        dotsClass: 'header-dots',
-    });
+$(function () {
+  $('.header__slider').slick({
+    arrows: false,
+    vertical: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    lazyLoad: 'progressive',
+    responsive: true,
+    dotsClass: 'header-dots',
+  });
 
-    $('.menu__btn').on('click', function(){
-        $('.header__menu-list').slideToggle();
-    });
+  $('.menu__btn').on('click', function () {
+    $('.header__menu-list').slideToggle();
+  });
 });
